036033b1949727c2185b81a866b6d779457eb30e
wyq <oak.hope@gmail.com>
2018-01-12 11:14:30 +0800

    v1.0.0

--- src/AliyunSMS.php
+++ src/AliyunSMS.php
@@ -1,99 +1,200 @@
 <?php
-/**
- * Created by wyq
- * Date: 17-6-2
- * Time: 下午3:37
- */
 
 namespace Oakhope;
 
-require_once __DIR__.'/aliyun-mns-php-sdk-1.3.4/mns-autoloader.php';
+use Aliyun\Core\Config;
+use Aliyun\Core\Profile\DefaultProfile;
+use Aliyun\Core\DefaultAcsClient;
+use Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
+use Aliyun\Api\Sms\Request\V20170525\QuerySendDetailsRequest;
 
-use Exception;
-use AliyunMNS\Client;
-use AliyunMNS\Model\BatchSmsAttributes;
-use AliyunMNS\Model\MessageAttributes;
-use AliyunMNS\Exception\MnsException;
-use AliyunMNS\Requests\PublishMessageRequest;
+// 加载区域结点配置
+Config::load();
 
 class AliyunSMS
 {
     private $accessId;
     private $accessKey;
-    private $endPoint;
-    private $client;
-    private $topicName;
     private $signName;
+    private static $acsClient = null;
 
+    /**
+     * AliyunSMS constructor.
+     * @param $accessId
+     * @param $accessKey
+     * @param string $endPoint @deprecated v2.0.0
+     * @param string $topicName @deprecated v2.0.0
+     * @param $signName
+     */
     public function __construct(
         $accessId,
         $accessKey,
-        $endPoint,
-        $topicName,
+        $endPoint = 'deprecated',
+        $topicName = 'deprecated',
         $signName
     ) {
-        /**
-         * Step 1. 初始化Client
-         */
         $this->accessId = $accessId;
         $this->accessKey = $accessKey;
-        $this->endPoint = $endPoint;  // eg. http://1234567890123456.mns.cn-shenzhen.aliyuncs.com
-        $this->topicName = $topicName;
         $this->signName = $signName;
-
-        $this->client = new Client(
-            $this->endPoint,
-            $this->accessId,
-            $this->accessKey
-        );
-
     }
 
-    public function sendOne($templateCode, $phone, array $templateParam = ['' => ''], $messageBody = 'empty') {
-
-        /**
-         * Step 2. 获取主题引用
-         */
-        $topic = $this->client->getTopicRef($this->topicName);
-        /**
-         * Step 3. 生成SMS消息属性
-         */
-        // 3.1 设置发送短信的签名（SMSSignName）和模板（SMSTemplateCode）
-        $batchSmsAttributes = new BatchSmsAttributes($this->signName, $templateCode);
-        // 3.2 （如果在短信模板中定义了参数）指定短信模板中对应参数的值
+    /**
+     * @param $templateCode
+     * @param $phoneNumbers
+     * @param array $templateParam
+     * @param string $messageBody @deprecated v2.0.0
+     * @param string $outId 可选，设置流水号
+     * @param string $smsUpExtendCode 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
+     * @return mixed
+        class stdClass#1245 (4) {
+            public $Message =>
+            string(2) "OK"
+            public $RequestId =>
+            string(36) "B1572C16-8C32-44EB-A497-B60E42EB1FCA"
+            public $BizId =>
+            string(20) "376905715726518670^0"
+            public $Code =>
+            string(2) "OK"
+        }
+     */
+    public function sendOne($templateCode, $phoneNumbers, array $templateParam = ['' => ''], $messageBody = null,  $outId = null, $smsUpExtendCode = null) {
 
         // 参数转为string避免报错
         $templateParam = array_map(
             function ($value) {
-                return strval($value);
+                return (string)$value;
             },
             $templateParam
         );
 
-        $batchSmsAttributes->addReceiver($phone, $templateParam);
-
-        $messageAttributes = new MessageAttributes(array($batchSmsAttributes));
-        /**
-         * Step 4. 设置SMS消息体（必须）
-         *
-         * 注：目前暂时不支持消息内容为空，需要指定消息内容，不为空即可。
-         */
-//        $messageBody = "smsmessage";
-
-        /**
-         * Step 5. 发布SMS消息
-         */
-        $request = new PublishMessageRequest($messageBody, $messageAttributes);
-        try
-        {
-            $res = $topic->publishMessage($request);
-            return $res->isSucceed();
-        }
-        catch (MnsException $e)
-        {
-            throw new Exception($e->getMessage().' [code:'.$e->getMnsErrorCode().']');
-        }
+        return $this->sendSms($phoneNumbers, $this->signName, $templateCode, $templateParam['code'], $templateParam['product'], $outId, $smsUpExtendCode);
     }
 
+    /**
+     * 取得AcsClient
+     *
+     * @return DefaultAcsClient
+     */
+    private function getAcsClient() {
+        //产品名称:云通信流量服务API产品,开发者无需替换
+        $product = "Dysmsapi";
 
+        //产品域名,开发者无需替换
+        $domain = "dysmsapi.aliyuncs.com";
+
+        $accessKeyId = $this->accessId; // AccessKeyId
+        $accessKeySecret = $this->accessKey; // AccessKeySecret
+
+        // 暂时不支持多Region
+        $region = "cn-hangzhou";
+
+        // 服务结点
+        $endPointName = "cn-hangzhou";
+
+        if(static::$acsClient === null) {
+
+            //初始化acsClient,暂不支持region化
+            $profile = DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
+
+            // 增加服务结点
+            DefaultProfile::addEndpoint($endPointName, $region, $product, $domain);
+
+            // 初始化AcsClient用于发起请求
+            static::$acsClient = new DefaultAcsClient($profile);
+        }
+
+        return static::$acsClient;
+    }
+
+    /**
+     * 发送短信
+     * @param $phoneNumbers array|string
+     * @param $signName string
+     * @param $templateCode string
+     * @param $code string
+     * @param $product string
+     * @param $outId string
+     * @param $smsUpExtendCode string
+     * @return mixed|\SimpleXMLElement
+        class stdClass#1245 (4) {
+           public $Message =>
+           string(2) "OK"
+           public $RequestId =>
+           string(36) "B1572C16-8C32-44EB-A497-B60E42EB1FCA"
+           public $BizId =>
+           string(20) "376905715726518670^0"
+           public $Code =>
+           string(2) "OK"
+        }
+     */
+    private function sendSms($phoneNumbers, $signName, $templateCode, $code, $product, $outId = null, $smsUpExtendCode = null) {
+
+        // 初始化SendSmsRequest实例用于设置发送短信的参数
+        $request = new SendSmsRequest();
+
+        // 必填，设置短信接收号码
+        $request->setPhoneNumbers($phoneNumbers);
+
+        // 必填，设置签名名称，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $request->setSignName($signName);
+
+        // 必填，设置模板CODE，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $request->setTemplateCode($templateCode);
+
+        // 可选，设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $request->setTemplateParam(json_encode(array(  // 短信模板中字段的值
+            'code' => $code,
+            'product' => $product
+        ), JSON_UNESCAPED_UNICODE));
+
+        // 可选，设置流水号
+        if ($outId) {
+            $request->setOutId($outId);
+        }
+
+        // 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
+        if ($smsUpExtendCode) {
+            $request->setSmsUpExtendCode($smsUpExtendCode);
+        }
+
+        // 发起访问请求 return $acsResponse
+        return $this->getAcsClient()->getAcsResponse($request);
+    }
+
+    /**
+     * 短信发送记录查询
+     * @param $phoneNumber
+     * @param $sendDate
+     * @param $pageSize
+     * @param $currentPage
+     * @param string $yourBizId
+     * @return mixed|\SimpleXMLElement
+     */
+    public function querySendDetails($phoneNumber, $sendDate, $pageSize, $currentPage, $yourBizId = "yourBizId") {
+
+        // 初始化QuerySendDetailsRequest实例用于设置短信查询的参数
+        $request = new QuerySendDetailsRequest();
+
+        // 必填，短信接收号码
+//        $request->setPhoneNumber('1234567');
+        $request->setPhoneNumber($phoneNumber);
+
+        // 必填，短信发送日期，格式Ymd，支持近30天记录查询
+//        $request->setSendDate("20170718");
+        $request->setSendDate($sendDate);
+
+        // 必填，分页大小
+//        $request->setPageSize(10);
+        $request->setPageSize($pageSize);
+
+        // 必填，当前页码
+//        $request->setCurrentPage(1);
+        $request->setCurrentPage($currentPage);
+
+        // 选填，短信发送流水号
+        $request->setBizId($yourBizId);
+
+        // 发起访问请求 return $acsResponse
+        return $this->getAcsClient()->getAcsResponse($request);
+    }
 }
--- src/test/test.php
+++ src/test/test.php
@@ -5,7 +5,7 @@
  * Time: 下午9:10
  */
 
-require_once(__DIR__.'/../../vendor/autoload.php');
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 // edit aliyun-mns-config.ini first
 $ini_array = parse_ini_file(__DIR__ . "/aliyun-mns-config.ini");
@@ -17,7 +17,6 @@ $signName = $ini_array["signName"];
 $templateCode = $ini_array["templateCode"];
 $phone = $ini_array["phone"];
 
-
 $aliyunSms = new \Oakhope\AliyunSMS($accessId, $accessKey, $endPoint, $topicName, $signName);
 
-echo $aliyunSms->sendOne($templateCode, $phone, ['code' => '123123', 'product' => 'appname']);
+var_dump($aliyunSms->sendOne($templateCode, $phone, ['code' => '123123', 'product' => 'appname']));
